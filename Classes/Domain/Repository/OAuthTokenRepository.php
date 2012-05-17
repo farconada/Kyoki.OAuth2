56be3b465920025959ca85299822ab869841640d
Fernando Arconada <fernando.arconada@gmail.com>
2012-05-17 10:25:41 +0200

    dummy repositories

---
+++ Classes/Domain/Repository/OAuthTokenRepository.php
@@ -0,0 +1,19 @@
+<?php
+namespace Kyoki\OAuth2\Domain\Repository;
+/**
+ * Created by JetBrains PhpStorm.
+ * User: fernando
+ * Date: 28/04/12
+ * Time: 13:24
+ * To change this template use File | Settings | File Templates.
+ */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+/**
+ * Repository for parties
+ *
+ * @FLOW3\Scope("singleton")
+ */
+class OAuthTokenRepository extends \TYPO3\FLOW3\Persistence\Repository {
+
+}
